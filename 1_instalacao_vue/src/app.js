b5fbf1ba5635b8079aa7d4f6fe96257ceca9ba51
João Victor Modesto da Cruz <joao.modesto@progen.com.br>
2021-11-25 09:21:41 -0300

    data binding

--- 1_instalacao_vue/src/app.js
+++ 1_instalacao_vue/src/app.js
@@ -1,10 +1,21 @@
 const MyNameApp = {
     data() {
         return {
-            name: "joao",
-            idade: 22
+            name: "",
+            idade: 22,
+            input_name:""
+        }
+    },
+    //link de vento por uma diretiva do vue
+    methods:{
+        submitForm(e){
+            e.preventDefault();
+
+            console.log(this.input_name);
+            this.name = this.input_name
         }
     }
 }
+
 
 Vue.createApp(MyNameApp).mount("#app");
